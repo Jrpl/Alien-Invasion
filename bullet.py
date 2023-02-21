528343ab1893ac9d684aca45321b4095ca63baec
Justin Pfeifler <jrpl101998@gmail.com>
2023-02-21 02:50:47 -0600

    Create bullet class

---
+++ bullet.py
@@ -0,0 +1,34 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    """A class to manage bullets fired."""
+
+    def __init__(self, ai_settings, screen, ship):
+        """Create a bullet at ship position."""
+        super(Bullet, self).__init__()
+        self.screen = screen
+
+        # Create a bullet rect at (0, 0) and then set position.
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
+
+        # Store the bullet's position
+        self.y = float(self.rect.y)
+
+        self.color = ai_settings.bullet_color
+        self.speed_factor = ai_settings.bullet_speed_factor
+
+
+    def update(self):
+        """Move bullet"""
+        # Update the decimal position
+        self.y -= self.speed_factor
+        # Update the rect
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
+
