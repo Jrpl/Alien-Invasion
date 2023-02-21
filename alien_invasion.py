f77d4d59829907e1c3ea6b286ed14a448c411a04
Justin Pfeifler <jrpl101998@gmail.com>
2023-02-21 02:52:17 -0600

    Add bullet updating to game loop

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -14,12 +15,15 @@ def run_game():
 
     # Make a ship.
     ship = Ship(ai_settings, screen)
+    # Make a group for bullets
+    bullets = Group()
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
